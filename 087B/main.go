49a8321a04497757c9fd4047dbccd7a55eac3b1c
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-16 23:39:47 +0900

    Update test

--- 087B/main.go
+++ 087B/main.go
@@ -2,23 +2,22 @@ package main
 
 import "fmt"
 
-func pattern(a, b, c, x int) int {
-	var pattern int
-	for aloop := 0; aloop <= a; aloop++ {
-		for bloop := 0; bloop <= b; bloop++ {
-			for cloop := 0; cloop <= c; cloop++ {
-				sum := 500*aloop + 100*bloop + 50*cloop
-				if sum == x {
-					pattern++
+func coins(a, b, c, x int) int {
+	var cnt int
+	for i := 0; i <= a; i++ {
+		for j := 0; j <= b; j++ {
+			for k := 0; k <= c; k++ {
+				if 500*i+100*j+50*k == x {
+					cnt++
 				}
 			}
 		}
 	}
-	return pattern
+	return cnt
 }
 
 func main() {
 	var a, b, c, x int
 	fmt.Scan(&a, &b, &c, &x)
-	fmt.Println(pattern(a, b, c, x))
+	fmt.Println(coins(a, b, c, x))
 }
