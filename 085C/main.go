83906a3f486155903cc16459e4d904132348ac88
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-18 01:36:29 +0900

    Update test

--- 085C/main.go
+++ 085C/main.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 )
 
-func dama(n, y int) (a, b, c int) {
-	d := make([]int, n)
-	a, b, c = -1, -1, -1
+func dama(n, y int) (int, int, int) {
+	d := make([]int, n+1)
+	a, b, c := -1, -1, -1
 	for i := range d {
 		for j := range d {
 			k := n - i - j
 			if k < 0 {
 				continue
 			}
-			if y == 10000*j+5000*j+1000*k {
+			if y == 10000*i+5000*j+1000*k {
 				a = i
 				b = j
 				c = k
+				return a, b, c
 			}
 		}
 	}
--- 085C/main_test.go
+++ 085C/main_test.go
@@ -1,11 +1,45 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestDama(t *testing.T) {
-	n, y := 9, 45000
-	wa, wb, wc := 4, 0, 5
-	if ga, gb, gc := dama(n, y); ga != wa || gb != wb || gc != wc {
-		t.Fatalf("want is %v %v %v , got is %v %v %v", wa, wb, wc, ga, gb, gc)
+	cases := map[string]struct {
+		n, y int
+		want []int
+	}{
+		"1": {
+			n:    9,
+			y:    45000,
+			want: []int{0, 9, 0},
+		},
+		"2": {
+			n:    20,
+			y:    196000,
+			want: []int{-1, -1, -1},
+		},
+		"3": {
+			n:    1000,
+			y:    1234000,
+			want: []int{2, 54, 944},
+		},
+		"4": {
+			n:    2000,
+			y:    20000000,
+			want: []int{2000, 0, 0},
+		},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			a, b, c := dama(tc.n, tc.y)
+			got := []int{a, b, c}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("want is %v , got is %v", tc.want, got)
+			}
+		})
 	}
 }
