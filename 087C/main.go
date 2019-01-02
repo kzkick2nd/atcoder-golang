5e1c30d82cf9bd08559b86c92b8d162bcff332b7
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-02 18:24:29 +0900

    Did 087C

--- 087C/main.go
+++ 087C/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Scan(&a[i])
+	}
+	b := make([]int, n)
+	for i := range b {
+		fmt.Scan(&b[i])
+	}
+	fmt.Println(seek(n, a, b))
+}
+
+func seek(n int, a, b []int) int {
+	var s int
+	for i := 0; i < n; i++ {
+		var sum int
+		for _, v := range a[:i+1] {
+			sum += v
+		}
+		for _, v := range b[i:] {
+			sum += v
+		}
+		if sum > s {
+			s = sum
+		}
+	}
+	return s
+}
