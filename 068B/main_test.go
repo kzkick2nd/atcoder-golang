c102805c9d2123c5009f9b6aed9e6c5a6545a0a2
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-18 01:06:30 +0900

    Add test 068B

---
+++ 068B/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestNum(t *testing.T) {
+	cases := map[string]struct {
+		n, expect int
+	}{
+		"1": {n: 7, expect: 4},
+		"2": {n: 32, expect: 32},
+		"3": {n: 1, expect: 1},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := num(tc.n); actual != tc.expect {
+				t.Fatalf("expect is %d, actual is %d", tc.expect, actual)
+			}
+		})
+	}
+}
