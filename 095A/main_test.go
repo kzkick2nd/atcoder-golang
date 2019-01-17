0b01d9a8e270f840c55b6023c53d3ee157c77e31
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-18 00:03:52 +0900

    Add 095A test

---
+++ 095A/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestOn(t *testing.T) {
+	cases := map[string]struct {
+		s      string
+		expect int
+	}{
+		"1": {s: "oxo", expect: 900},
+		"2": {s: "ooo", expect: 1000},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := on(tc.s); actual != tc.expect {
+				t.Fatalf("expect is %d, acutual is %d", tc.expect, actual)
+			}
+		})
+	}
+}
