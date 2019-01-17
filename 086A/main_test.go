2613279768d51fce401157fd48f2ecc8b86c412c
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-17 23:15:56 +0900

    fix wording

--- 086A/main_test.go
+++ 086A/main_test.go
@@ -4,18 +4,18 @@ import "testing"
 
 func TestEvenodd(t *testing.T) {
 	cases := map[string]struct {
-		a, b int
-		want string
+		a, b   int
+		expect string
 	}{
-		"1": {a: 3, b: 4, want: "Even"},
-		"2": {a: 1, b: 21, want: "Odd"},
+		"1": {a: 3, b: 4, expect: "Even"},
+		"2": {a: 1, b: 21, expect: "Odd"},
 	}
 
 	for n, tc := range cases {
 		tc := tc
 		t.Run(n, func(t *testing.T) {
-			if got := evenodd(tc.a * tc.b); got != tc.want {
-				t.Fatalf("want = %v, got = %v", tc.want, got)
+			if actual := evenodd(tc.a * tc.b); actual != tc.expect {
+				t.Fatalf("expect = %v, actual = %v", tc.expect, actual)
 			}
 		})
 	}
