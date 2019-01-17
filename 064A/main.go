acaad8d1b9ca3c07455822b7512b267010d5b63a
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-17 23:35:14 +0900

    Did 064A

--- 064A/main.go
+++ 064A/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	input := make([]int, 3)
-	for i := range input {
-		fmt.Scan(&input[i])
+	in := make([]int, 3)
+	for i := range in {
+		fmt.Scan(&in[i])
 	}
-	fmt.Println(rgb(input))
+	fmt.Println(rgb(in))
 }
 
-func rgb(rgb []int) string {
-	return "YES"
+func rgb(in []int) string {
+	s := 100*in[0] + 10*in[1] + in[2]
+	if s%4 == 0 {
+		return "YES"
+	}
+	return "NO"
 }
