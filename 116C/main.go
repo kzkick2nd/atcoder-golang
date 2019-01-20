bcc0dbf83470e67907bebbf315255b4bd9616e4b
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-20 21:37:01 +0900

    Add empty function

--- 116C/main.go
+++ 116C/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	h := make([]int, n)
+	for n := range h {
+		fmt.Scan(&h[n])
+	}
+	fmt.Println(garden(h))
+}
+
+func garden(h []int) int {
+	return 0
+}
