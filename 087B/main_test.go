362aa4543fb0185721412293920f1f5129e5305b
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-20 21:01:52 +0900

    Add 116A empty func

--- 087B/main_test.go
+++ 087B/main_test.go
@@ -5,18 +5,18 @@ import "testing"
 func TestPattern(t *testing.T) {
 	cases := map[string]struct {
 		a, b, c, x int
-		want       int
+		expect     int
 	}{
-		"1": {a: 2, b: 2, c: 2, x: 100, want: 2},
-		"2": {a: 5, b: 1, c: 0, x: 150, want: 0},
-		"3": {a: 30, b: 40, c: 50, x: 6000, want: 213},
+		"1": {a: 2, b: 2, c: 2, x: 100, expect: 2},
+		"2": {a: 5, b: 1, c: 0, x: 150, expect: 0},
+		"3": {a: 30, b: 40, c: 50, x: 6000, expect: 213},
 	}
 
 	for n, tc := range cases {
 		tc := tc
 		t.Run(n, func(t *testing.T) {
-			if got := coins(tc.a, tc.b, tc.c, tc.x); got != tc.want {
-				t.Fatalf("want is %v, got is %v", tc.want, got)
+			if actual := coins(tc.a, tc.b, tc.c, tc.x); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
 			}
 		})
 	}
