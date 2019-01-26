8dade526d884e4c6bd36739de0d9bd8732f307aa
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:27:11 +0900

    did 080B

--- 080B/main.go
+++ 080B/main.go
@@ -1,7 +1,26 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(harshad(n))
+}
 
 func harshad(n int) string {
-	return "Yes"
+	f := digitSum(n)
+	if n%f == 0 {
+		return "Yes"
+	}
+	return "No"
+}
+
+func digitSum(n int) int {
+	var s int
+	for n > 0 {
+		s += n % 10
+		n /= 10
+	}
+	return s
 }
