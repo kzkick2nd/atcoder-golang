f62c5e638549618caa2c6745bd8fd0a302213140
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:51:37 +0900

    change test var name

--- 085B/main_test.go
+++ 085B/main_test.go
@@ -4,20 +4,20 @@ import "testing"
 
 func TestMochi(t *testing.T) {
 	cases := map[string]struct {
-		n    int
-		d    []int
-		want int
+		n      int
+		d      []int
+		expect int
 	}{
-		"1": {n: 4, d: []int{10, 8, 8, 6}, want: 3},
-		"2": {n: 3, d: []int{15, 15, 15}, want: 1},
-		"3": {n: 7, d: []int{50, 30, 50, 100, 50, 80, 30}, want: 4},
+		"1": {n: 4, d: []int{10, 8, 8, 6}, expect: 3},
+		"2": {n: 3, d: []int{15, 15, 15}, expect: 1},
+		"3": {n: 7, d: []int{50, 30, 50, 100, 50, 80, 30}, expect: 4},
 	}
 
 	for n, tc := range cases {
 		tc := tc
 		t.Run(n, func(t *testing.T) {
-			if got := bucket(tc.d); got != tc.want {
-				t.Fatalf("want is %v, got is %v", tc.want, got)
+			if actual := bucket(tc.d); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
 			}
 		})
 	}
