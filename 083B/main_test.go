362e1f383e2deb15bc6052d8a1103a06b9d50461
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:14:39 +0900

    Change test word

--- 083B/main_test.go
+++ 083B/main_test.go
@@ -4,18 +4,18 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	cases := map[string]struct {
-		n, a, b, want int
+		n, a, b, expect int
 	}{
-		"1": {n: 20, a: 2, b: 5, want: 84},
-		"2": {n: 10, a: 1, b: 2, want: 13},
-		"3": {n: 100, a: 4, b: 16, want: 4554},
+		"1": {n: 20, a: 2, b: 5, expect: 84},
+		"2": {n: 10, a: 1, b: 2, expect: 13},
+		"3": {n: 100, a: 4, b: 16, expect: 4554},
 	}
 
 	for n, tc := range cases {
 		tc := tc
 		t.Run(n, func(t *testing.T) {
-			if got := sum(tc.n, tc.a, tc.b); got != tc.want {
-				t.Fatalf("want is %v, got is %v", tc.want, got)
+			if actual := sum(tc.n, tc.a, tc.b); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
 			}
 		})
 	}
