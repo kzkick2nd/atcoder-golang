2785cdcf1e8c24873daa632f433fd5eef71bc328
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:03:21 +0900

    Add 105B test

---
+++ 105B/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestDonuts(t *testing.T) {
+	cases := map[string]struct {
+		n      int
+		expect string
+	}{
+		"1": {n: 11, expect: "Yes"},
+		"2": {n: 40, expect: "Yes"},
+		"3": {n: 3, expect: "No"},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := donuts(n); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v.", tc.expect, actual)
+			}
+		})
+	}
+}
