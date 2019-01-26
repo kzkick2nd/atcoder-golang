16c6de9531c495fd9407270b086f80d11fed0e42
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:31:17 +0900

    change test var name

--- 088B/main_test.go
+++ 088B/main_test.go
@@ -4,20 +4,20 @@ import "testing"
 
 func TestGame(t *testing.T) {
 	cases := map[string]struct {
-		n    int
-		a    []int
-		want int
+		n      int
+		a      []int
+		expect int
 	}{
-		"1": {n: 2, a: []int{3, 1}, want: 2},
-		"2": {n: 3, a: []int{2, 7, 4}, want: 5},
-		"3": {n: 4, a: []int{20, 18, 2, 18}, want: 18},
+		"1": {n: 2, a: []int{3, 1}, expect: 2},
+		"2": {n: 3, a: []int{2, 7, 4}, expect: 5},
+		"3": {n: 4, a: []int{20, 18, 2, 18}, expect: 18},
 	}
 
 	for n, tc := range cases {
 		tc := tc
 		t.Run(n, func(t *testing.T) {
-			if got := game(tc.n, tc.a); got != tc.want {
-				t.Fatalf("want is %v, got is %v", tc.want, got)
+			if actual := game(tc.n, tc.a); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
 			}
 		})
 	}
