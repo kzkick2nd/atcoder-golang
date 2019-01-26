b2d14643e1c67da463b201fd44bcd3dff0c5d9e4
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-27 00:44:49 +0900

    did 067B

--- 067B/main.go
+++ 067B/main.go
@@ -1,7 +1,25 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sort"
+)
 
-func sunuke(n int, l []int) int {
-	return 0
+func main() {
+	var n, k int
+	fmt.Scan(&n, &k)
+	l := make([]int, n)
+	for i := range l {
+		fmt.Scan(&l[i])
+	}
+	fmt.Println(sunuke(k, l))
+}
+
+func sunuke(k int, l []int) int {
+	var s int
+	sort.Sort(sort.Reverse(sort.IntSlice(l)))
+	for i := 0; i < k; i++ {
+		s += l[i]
+	}
+	return s
 }
