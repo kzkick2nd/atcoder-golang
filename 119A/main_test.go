5b3d9a763c2881bc538a0b334ce4d965021d62d9
kzkick2nd <aruga.kazuki@gmail.com>
2019-03-12 03:53:09 +0900

    Add 119A test

---
+++ 119A/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestHeisei(t *testing.T) {
+	cases := map[string]struct {
+		s      string
+		expect string
+	}{
+		"1": {s: "2019/04/30", expect: "Heisei"},
+		"2": {s: "2019/11/01", expect: "TBD"},
+	}
+
+	for n, tc := range cases {
+		t.Run(n,func(t *testing.T){
+			tc := tc
+			if( actual := heisei(tc.s); actual != tc.expect ){
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
+			}
+		})
+	}
+}
