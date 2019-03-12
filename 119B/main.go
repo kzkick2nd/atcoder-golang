ee12a4de01a2af5312041ddddf5eb05f5075b6e4
kzkick2nd <aruga.kazuki@gmail.com>
2019-03-13 04:05:19 +0900

    did 119B

--- 119B/main.go
+++ 119B/main.go
@@ -1,12 +1,30 @@
 package main
 
+import "fmt"
+
 type pair struct {
 	x float64
 	y string
 }
 
-func main() {}
+func main() {
+	var n int
+	fmt.Scan(&n)
+	p := make([]pair, n)
+	for i := range p {
+		fmt.Scan(&p[i].x, &p[i].y)
+	}
+	fmt.Println(yen(p))
+}
 
 func yen(p []pair) float64 {
-	return 1.0
+	var sum float64
+	for _, v := range p {
+		if v.y == "BTC" {
+			sum += v.x * 380000.0
+		} else {
+			sum += v.x
+		}
+	}
+	return sum
 }
