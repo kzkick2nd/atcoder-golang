20d688203b0c0fd472bc8e616bcf5403af415715
kzkick2nd <aruga.kazuki@gmail.com>
2019-03-13 04:00:01 +0900

    Add 119B test

--- 119B/main.go
+++ 119B/main.go
@@ -1,3 +1,12 @@
 package main
 
+type pair struct {
+	x float64
+	y string
+}
+
 func main() {}
+
+func yen(p []pair) float64 {
+	return 1.0
+}
--- 119B/main_test.go
+++ 119B/main_test.go
@@ -2,4 +2,21 @@ package main
 
 import "testing"
 
-func TestYen(t *testing.T) {}
+func TestYen(t *testing.T) {
+	cases := map[string]struct {
+		p      []pair
+		expect float64
+	}{
+		"1": {p: []pair{{x: 10000, y: "JPY"}, {x: 0.10000000, y: "BTC"}}, expect: 48000.0},
+		"2": {p: []pair{{x: 100000000, y: "JPY"}, {x: 100.00000000, y: "BTC"}, {x: 0.00000001, y: "BTC"}}, expect: 138000000.0038},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := yen(tc.p); actual != tc.expect {
+				t.Fatalf("expect is %v, actual is %v", tc.expect, actual)
+			}
+		})
+	}
+}
