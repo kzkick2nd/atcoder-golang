624ea124b396866226479af8b90a03ba82c49ef7
kzkick2nd <aruga.kazuki@gmail.com>
2019-04-08 02:19:07 +0900

    WA 120B

--- 120B/main.go
+++ 120B/main.go
@@ -1,9 +1,26 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var a, b, k int
+	fmt.Scan(&a, &b, &k)
+	fmt.Println(divisor(a, b, k))
+}
 
 func divisor(a, b, k int) int {
-	return 0
+	g := gcd(a, b)
+	c := 1
+	for g > 1 {
+		g--
+		if a%g == 0 && b%g == 0 {
+			c++
+			if c == k {
+				return g
+			}
+		}
+	}
+	return 1
 }
 
 func gcd(a, b int) int {
