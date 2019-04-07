e4860f7a76df6d1a82193693a69247cfa72c34ca
kzkick2nd <aruga.kazuki@gmail.com>
2019-04-08 01:56:04 +0900

    Add test 120B

--- 120B/main.go
+++ 120B/main.go
@@ -1,3 +1,14 @@
 package main
 
 func main() {}
+
+func divisor(a, b, k int) int {
+	return 0
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
--- 120B/main_test.go
+++ 120B/main_test.go
@@ -2,4 +2,22 @@ package main
 
 import "testing"
 
-func TestDiv(t *testing.T) {}
+func TestDiv(t *testing.T) {
+	cases := map[string]struct {
+		a, b, k  int
+		expected int
+	}{
+		"1": {a: 8, b: 12, k: 2, expected: 2},
+		"2": {a: 100, b: 50, k: 4, expected: 5},
+		"3": {a: 1, b: 1, k: 1, expected: 1},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := divisor(tc.a, tc.b, tc.k); tc.expected != actual {
+				t.Fatalf("expect is %v, actual is %v", tc.expected, actual)
+			}
+		})
+	}
+}
