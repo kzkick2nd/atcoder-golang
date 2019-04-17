f6da26dd3e29d99a041786d228ef515974f78b82
kzkick2nd <aruga.kazuki@gmail.com>
2019-04-17 17:02:08 +0900

    add test 121B

--- 121B/main_test.go
+++ 121B/main_test.go
@@ -2,4 +2,20 @@ package main
 
 import "testing"
 
-func TestSolve(t *testing.T) {}
+func TestSolve(t *testing.T) {
+	cases := map[string]struct {
+		n, m, c int
+		b       []int
+		a1      []int
+		a2      []int
+	}{
+		"1": {n: 2, m: 3, c: -10, b: []int{1, 2, 3}, a1: []int{3, 2, 1}, a2: []int{1, 2, 2}},
+	}
+
+	for tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			if actual := solv()
+		})
+	}
+}
